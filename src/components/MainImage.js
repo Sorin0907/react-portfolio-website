2bbafafeed812fd7f5dc41e553679191b4631a75
sorin.adamoiu <sorin.adamoiu@gmail.com>
2023-09-28 22:29:14 +0000

    feat: add background image and styling

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,24 +1,28 @@
 import "./MainImage.css";
 
-import React from 'react'
-import FrontImg from "../fixtures/FrontImg.png"
+import React from "react";
+import FrontImg2 from "../fixtures/FrontImg2.jpg";
 import { Link } from "react-router-dom";
 
 const MainImage = () => {
   return (
     <div className="main-image">
-        <div className="cover">
-            <img className="front-img" src={FrontImg} alt="frontImg"/>
-        </div>
-        <div className="content">
-            <p>HI, I'M A SOFTWARE DEVELOPER</p>
-        </div>
+      <div className="cover">
+        <img className="front-img" src={FrontImg2} alt="frontImg" />
+      </div>
+      <div className="content">
+        <p>HI, I'M A SOFTWARE DEVELOPER</p>
         <div>
-            <Link to="/portfolio" className="btn">Portfolio</Link>
-            <Link to="/portfolio" className="btn btn2">Contact</Link>
+          <Link to="/portfolio" className="btn">
+            Portfolio
+          </Link>
+          <Link to="/contact" className="btn btn2">
+            Contact
+          </Link>
         </div>
+      </div>
     </div>
   );
 };
 
-export default MainImage
+export default MainImage;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,52 @@
-import { Link } from "react-router-dom"
-import "./Navbar.css"
-import { FaBars, FaTimes } from "react-icons/fa"
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 import React, { useState } from "react";
 
 const Navbar = () => {
-
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
 
+  const [color, setColor] = useState(false);
+  const changeColor = () => {
+    if (window.scrollY >= 100) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeColor);
+
   return (
-    <div className="header">
-        <Link to="/">
-            <h1>Sorin Adamoiu</h1>
-        </Link>
-        <ul className={click ? "nav active" : "nav"}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-        <div className="menu" onClick ={handleClick}>
-          {click ? (
-          <FaTimes 
-            size={20}
-            style={{ color: "#fff" }}
-          /> 
-          ) : (
-          <FaBars
-            size={20}
-            style={{ color: "#fff" }}
-          />
-          )}
-        </div>
+    <div className={color ? "header header-bg" : "header"}>
+      <Link to="/">
+        <h1>Sorin Adamoiu</h1>
+      </Link>
+      <ul className={click ? "nav active" : "nav"}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="menu" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
     </div>
   );
 };
 
-export default Navbar
+export default Navbar;
