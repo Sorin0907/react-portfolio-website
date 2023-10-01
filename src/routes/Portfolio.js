6a9125014475fb3c73dd09f5c00bf4ba50a80385
sorin.adamoiu <sorin.adamoiu@gmail.com>
2023-10-01 12:59:23 +0000

    feat: creating background for About, Contact, Portfolio

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import MainImg2 from "../components/MainImg2";
 
 const Portfolio = () => {
   return (
     <div>
       <Navbar />
+      <MainImg2 heading="Portfolio" text="My most recent projects" />
       <Footer />
     </div>
   );
